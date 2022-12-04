57258cb7834e009d6a8cca620c4d93b4c4dea938
Sabid Mahmud <sabidmahmud96@gmail.com>
2022-12-04 14:37:50 +0600

    code update

--- budget.py
+++ budget.py
@@ -1,48 +1,75 @@
 class Category:
     def __init__(self, category_name):
-        self.cat_name = category_name
-    
-    def deposit(self, amount, details = None):
-        self.balance = amount
-        self.deposit_type = details
+        self.category_name = category_name
+        self.ledger = []
+        self.__balance = 0.00
+    def deposit(self, amount, details=""):
+        self.ledger.append({"amount": amount, "description": details})
+        self.__balance += amount
     def withdraw(self, amount = 0, details = ''):
-        self.withdraw_amount = amount
-        self.balance -= self.withdraw_amount
-        self.withdraw_details = details
-    
+        if self.check_funds(amount):
+            self.ledger.append({"amount": amount*-1, "description": details})
+            self.__balance -= amount
+            return True
+        return False
     def transfer(self, amount, where):
-        self.transfer_amount = amount
-        where.balance += self.transfer_amount
-
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {where.category_name}")
+            where.deposit(amount, f"Transfer from {self.category_name}")
+            return True
+        return False
     def get_balance(self):
-        return (self.balance)
+        return (self.__balance)
+    def check_funds(self, amount):
+        if self.__balance > amount:
+            return True
+        return False
 
-    def __sub__(self, other):
-        pass
-    def __add__(self, other):
-        pass  
+    def __str__(self):
+        header = self.category_name.center(30, "*") + "\n"
+        ledger = ""
+        for item in self.ledger:
+            # format description and amount
+            line_description = "{:<23}".format(item["description"])
+            line_amount = "{:>7.2f}".format(item["amount"])
+            # Truncate ledger description and amount to 23 and 7 characters respectively
+            ledger += "{}{}\n".format(line_description[:23], line_amount[:7])
+        total = "Total: {:.2f}".format(self.__balance)
+        return header + ledger + total
 
 
+def create_spend_chart(categories):
+    spent_amounts = []
+    # Get total spent in each category
+    for cat in categories:
+        spent = 0
+        for item in cat.ledger:
+            if item["amount"] < 0:
+                spent += abs(item["amount"])
+        spent_amounts.append(round(spent, 2))
 
+    # Calculate percentage rounded down to the nearest 10
+    total = round(sum(spent_amounts), 2)
+    spent_percentage = list(map(lambda amount: int((((amount / total) * 10) // 1) * 10), spent_amounts))
 
+    # Create the bar chart substrings
+    header = "Percentage spent by category\n"
 
+    chart = ""
+    for value in reversed(range(0, 101, 10)):
+        chart += str(value).rjust(3) + '|'
+        for percent in spent_percentage:
+            if percent >= value:
+                chart += " o "
+            else:
+                chart += "   "
+        chart += " \n"
 
+    footer = "    " + "-" * ((3 * len(categories)) + 1) + "\n"
+    descriptions = list(map(lambda category: cat.category_name, categories))
+    max_length = max(map(lambda description: len(description), descriptions))
+    descriptions = list(map(lambda description: description.ljust(max_length), descriptions))
+    for x in zip(*descriptions):
+        footer += "    " + "".join(map(lambda s: s.center(3), x)) + " \n"
 
-# def create_spend_chart(categories):
-#     pass 
-
-food = Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
-
-print(food)
-print(clothing)
+    return (header + chart + footer).rstrip("\n")
